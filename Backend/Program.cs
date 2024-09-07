7cacdb73f5a35fa0c37166804e91905e782291eb
Jakob-SA <agergaardskovjakob@gmail.com>
2024-09-07 12:06:48 +0200

    Tried connection postgreSQL, have not tested, should run.

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -4,6 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+//Attempt at connecting to the database
+// Add services to the container.
+// builder.Services.AddDbContext<MyDbContext>(options =>
+//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
@@ -23,7 +30,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
