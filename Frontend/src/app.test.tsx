80f4b09a3f9e9a32514c896cec0bc4250e135271
Jakob-SA <agergaardskovjakob@gmail.com>
2024-09-15 00:52:45 +0200

    removed test for vite default page and imports

--- Frontend/src/app.test.tsx
+++ Frontend/src/app.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/react";
+import { describe, it } from "vitest";
 import App from "./App";
 
 describe(App.name, () => {
