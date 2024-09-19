b1d01e1f5de149d9e686b81851344aaf1debfbe9
Fathionsons <gathefathe29@gmail.com>
2024-09-19 11:57:21 +0200

    Removing Winnerpage from Paymentpage branch
    Removing Winnerpage from Paymentpage branch

--- Frontend/src/components/Routes.tsx
+++ Frontend/src/components/Routes.tsx
@@ -2,7 +2,6 @@ import { RouteObject } from "react-router-dom";
 import SupportPage from "../views/support/support-page";
 import Home from "../views/home/home";
 import { Layout } from "./Layout";
-import WinnerPage from "@/views/WinnerPage/WinnerPage";
 import PaymentPage from "@/views/PaymentPage/PaymentPage";
 
 export const routes: RouteObject[] = [
@@ -17,10 +16,6 @@ export const routes: RouteObject[] = [
       {
         path: "/support",
         element: <SupportPage />,
-      },
-      {
-        path: "/winnerpage",
-        element: <WinnerPage winnerName={"Lars"} itemTitle={"Chessboard"} auctionEndDate={"12-25-2024"} />,
       },
       {
         path: "/paymentPage",
