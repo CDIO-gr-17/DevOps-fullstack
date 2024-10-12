6ee0950ae5403034f012db1275601e556c5d8003
Jakob-SA <agergaardskovjakob@gmail.com>
2024-10-12 23:16:13 +0200

    added redux and wrapped app. Only logic for a counter in state

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./navigation/Routes";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const router = createBrowserRouter(routes);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
