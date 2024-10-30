8e637595c90af860d837f5ccd853eaa3b065ddfb
Johan Holmsteen <johan@holmsteen.dev>
2024-10-30 21:19:41 +0100

    refac(CategoryJunction): Now makes use of a simple primary key

--- Backend/Models/CategoryJunction.cs
+++ Backend/Models/CategoryJunction.cs
@@ -3,6 +3,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 public class CategoryJunction
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
     [Required]
     [ForeignKey("ItemId")]
     public int ItemId { get; set; }
