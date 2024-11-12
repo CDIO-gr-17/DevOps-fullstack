056f4dd698a55befb1f1b2168965ea20244d2fd5
Johan Holmsteen <johan@holmsteen.dev>
2024-11-12 19:57:06 +0100

    fix(auctionwares): changed the name of the auctionsware controller

--- Backend/Controllers/AuctionWareController.cs
+++ Backend/Controllers/AuctionWareController.cs
@@ -3,25 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AuctionsController : ControllerBase
+public class AuctionWaresController : ControllerBase
 {
     private readonly DatabaseContext _context;
 
-    public AuctionsController(DatabaseContext context)
+    public AuctionWaresController(DatabaseContext context)
     {
         _context = context;
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreateAuction([FromBody] AuctionWare auctionware)
+    public async Task<IActionResult> CreateAuctionWare([FromBody] AuctionWare auctionware)
     {
         _context.AuctionWare.Add(auctionware);
         await _context.SaveChangesAsync();
-        return CreatedAtAction(nameof(GetAuction), new { id = auctionware.ItemId }, auctionware);
+        return CreatedAtAction(nameof(GetAuctionWare), new { id = auctionware.ItemId }, auctionware);
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetAuction(int id)
+    public async Task<IActionResult> GetAuctionWare(int id)
     {
         var auction = await _context.AuctionWare.FindAsync(id);
         if (auction == null)
@@ -32,7 +32,7 @@ public class AuctionsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAuctions()
+    public async Task<IActionResult> GetAuctionWares()
     {
         var currentDateTime = DateTime.UtcNow;              //aware of timezones and conversion when people submit auctions
         var auctions = await _context.AuctionWare
