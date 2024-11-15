d2c597513e4fdb2aa696da887b2eea6e0b710ff9
Jakob-SA <agergaardskovjakob@gmail.com>
2024-11-15 19:10:45 +0100

    corrected error

--- Frontend/src/services/customerService.ts
+++ Frontend/src/services/customerService.ts
@@ -13,11 +13,7 @@ export interface Customer {
 export interface Createdcustomer extends Customer {
   id: number;
 }
-export interface Createdcustomer extends Customer {
-  id: number;
-}
 
-const API_URL = "http://51.120.6.166/api/customers";
 const API_URL = "http://51.120.6.166/api/customers";
 
 export const createCustomer = async (customer: Customer) => {
@@ -31,22 +27,7 @@ export const createCustomer = async (customer: Customer) => {
   }
 };
 
-export const getCustomers = async (id: number) => {
-  const url = id ? `${API_URL}/${id}` : API_URL;
-  const response = await axios.get(url);
-  return response.data;
-};
-  try {
-    const response = await axios.post(API_URL, customer, {
-      timeout: 10000,
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const getCustomers = async (id: number) => {
+export const getCustomers = async (id?: number) => {
   const url = id ? `${API_URL}/${id}` : API_URL;
   const response = await axios.get(url);
   return response.data;
