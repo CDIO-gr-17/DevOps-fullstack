c587df898ac4c41f0ea3562aa0a08181ec1699f2
Jakob-SA <agergaardskovjakob@gmail.com>
2024-11-15 19:12:37 +0100

    Squashed commit of the following:
    commit 596918964417f42d352218ebe970b994c95901a4
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 16:22:40 2024 +0100
    
        Added proper back
    
    commit a85224a6be4e6ec99be18af39ae0648759a3e178
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 16:20:40 2024 +0100
    
        added them back
    
    commit 202fbf4f856ff56d031c2c137be2da0bd46cb793
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 16:17:39 2024 +0100
    
        Removed cors headers from nginx
    
    commit 598eaa6cbc8dfece2404e160198bfd3aca8b39ff
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 16:04:07 2024 +0100
    
        Changed origins permissions
    
    commit bccb8f9f1cd74a763f18a86cdf3c7cc92a74861d
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 16:04:07 2024 +0100
    
        Changed origins permissions
    
    commit 01bfcb2616b0e29f9104760ad3b62a9342ceb095
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 15:45:17 2024 +0100
    
        Returns all auctions
    
    commit 3af266c55c437e6f6fabb3f11273aa17e089c48d
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 14:38:42 2024 +0100
    
        Changed compose file
    
    commit fade293aee275ae4e2e112d8a54bb7c14b6a3d84
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 13:23:50 2024 +0100
    
        Added database to healthcheck
    
    commit 90c0b72d1aec3da110a8e6c09375457a9cc45a5d
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 10:32:06 2024 +0100
    
        back to host backend
    
    commit 168361318aeb630a2fbc2d87f3a386a38ffbff0b
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 09:40:26 2024 +0100
    
        Change to conf
    
    commit 0e046e82c6d4019f667b7814d8ebe8ff5bf4b0f7
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Thu Nov 14 09:33:22 2024 +0100
    
        Added database migration files to git to resolve incorrect database in prod
    
    commit bcebf696af6c80e966158658e3170c68464ff1ab
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 21:40:38 2024 +0100
    
        bakcned proxy
    
    commit 242f44b507d6e3cf5a0288f3824342bbeefeb00b
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 21:35:26 2024 +0100
    
        try to accept http on backend
    
    commit 74c68bffc972235a4c37f18c05b5d4de1ca31538
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 21:21:46 2024 +0100
    
        Removed https redirection
    
    commit 57e68975cec2c4c577e6da17d408c05272e30c31
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 21:14:58 2024 +0100
    
        changed back to old conf
    
    commit 15b018e3deb175c545765ed958c4e9196b79ea9b
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 21:05:07 2024 +0100
    
        Removed 8080
    
    commit b4dc4efeaf27b08950f3cac8330c1e7509eeea78
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 20:45:56 2024 +0100
    
        another oneone
    
    commit 2c4aa16ecfff0d3ca362c80b0295d539cb46dfe1
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 20:41:19 2024 +0100
    
        another one
    
    commit 31c7d488c9630fdae114cdd6b588358ae9ab9ca9
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 20:39:13 2024 +0100
    
        Changed back
    
    commit fcd76ac18a575d3a0d606e748026dd91221132bc
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 20:34:05 2024 +0100
    
        update to nginx conf
    
    commit 488cadfb9412372e7cd4edaef7b14e05fe57d9a7
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 20:22:31 2024 +0100
    
        Change in the nginx.conf
    
    commit af66837d2011ca4faf1206219d5cabd4c52e6e52
    Author: Jakob-SA <agergaardskovjakob@gmail.com>
    Date:   Tue Nov 12 20:10:43 2024 +0100
    
        adjusted static api url

--- Frontend/src/services/customerService.ts
+++ Frontend/src/services/customerService.ts
@@ -1,6 +1,14 @@
 import axios from "axios";
 
 export interface Customer {
+  FirstName: string;
+  LastName: string;
+  Email: string;
+  Password: string;
+  Address: string;
+  PhoneNumber: string;
+  AccountType: string;
+}
   firstName: string;
   lastName: string;
   email: string;
@@ -13,10 +21,29 @@ export interface Customer {
 export interface Createdcustomer extends Customer {
   id: number;
 }
+export interface Createdcustomer extends Customer {
+  id: number;
+}
 
+const API_URL = "http://51.120.6.166/api/customers";
 const API_URL = "http://51.120.6.166/api/customers";
 
 export const createCustomer = async (customer: Customer) => {
+  try {
+    const response = await axios.post(API_URL, customer, {
+      timeout: 10000,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getCustomers = async (id: number) => {
+  const url = id ? `${API_URL}/${id}` : API_URL;
+  const response = await axios.get(url);
+  return response.data;
+};
   try {
     const response = await axios.post(API_URL, customer, {
       timeout: 10000,
