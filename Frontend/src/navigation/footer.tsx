db0ffd020f38357b01654e876294c5a0a07d226a
Jakob-SA <agergaardskovjakob@gmail.com>
2024-11-15 14:22:21 +0100

    adjusted footer

--- Frontend/src/navigation/footer.tsx
+++ Frontend/src/navigation/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 w-screen z-10 flex items-center justify-center h-auto p-2 bg-gray-800 text-white">
+    <footer className="bottom-0 left-0 w-screen z-10 flex items-center justify-center h-auto p-2 bg-gray-800 text-white">
       <p>&copy; 2024 KunstHavn ApS</p>
     </footer>
   );
