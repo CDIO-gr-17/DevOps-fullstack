6c50c5a4757cc5c10009b3b1b34736fe5b0f171d
Jossemand <s224564@student.dtu.dk>
2024-11-24 17:55:20 +0100

    Refactor auth0Profile and createAuction views

--- Frontend/src/auth0Profile.tsx
+++ Frontend/src/auth0Profile.tsx
@@ -1,12 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-  const [userMetadata, setUserMetadata] = useState(null);
 
   useEffect(() => {
-    const getUserMetadata = async () => {
+    const getUserToken = async () => {
       const domain = "dev-ilmn83jst05snfbb.us.auth0.com";
 
       try {
@@ -16,46 +15,22 @@ const Profile = () => {
             scope: "read:current_user",
           },
         });
-
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
-
-        const metadataResponse = await fetch(userDetailsByIdUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        const { user_metadata } = await metadataResponse.json();
-
-        setUserMetadata(user_metadata);
+        console.log("Access token: " + accessToken);
       } catch (e) {
-        console.log(e.message);
-        console.log("User object:", user);
+        console.log((e as Error).message);
       }
     };
 
-    getUserMetadata();
+    getUserToken();
   }, [getAccessTokenSilently, user?.sub]);
 
   return (
     isAuthenticated && (
       <div>
-        <img src={user.picture} alt={user.name} />
-        <h3 className="text-4xl font-bold">Welcome, {user.name}</h3>
-
-        <h2>User name: {user.name}</h2>
-        <p>User emial: {user.email}</p>
-        {userMetadata ? (
-          <div>
-            <p>First name: {userMetadata.first_name}</p>
-            <p>Last name: {userMetadata.last_name}</p>
-            <p>Account Type: {userMetadata.account_type}</p>
-            <p>Address: {userMetadata.address}</p>
-            <p>Phone Number: {userMetadata.phone_number.number}</p>
-          </div>
-        ) : (
-          "No user metadata defined"
-        )}
+        {user && <img src={user.picture} alt={user.name} />}
+        <h3 className="text-4xl font-bold">Welcome, {user?.name}</h3>
+        <h2>User name: {user?.name ?? "Unknown"}</h2>
+        <p>User email: {user?.email}</p>
       </div>
     )
   );
