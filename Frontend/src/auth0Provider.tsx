b761453acdd3103fbb4bd4c20d4ae0714277b486
Philip Muff <s224566@dtu.dk>
2024-11-24 12:33:25 +0100

    get metadata silently (needs implementation from backend)

--- Frontend/src/auth0Provider.tsx
+++ Frontend/src/auth0Provider.tsx
@@ -8,7 +8,9 @@ root.render(
     domain="dev-ilmn83jst05snfbb.us.auth0.com"
     clientId="330cs4wFl6J9CqBLi1eaklhmPKf3ShH4"
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
+      audience: "https://dev-ilmn83jst05snfbb.us.auth0.com/api/v2/",
+      scope: "read:current_user update:current_user_metadata"
     }}
   >
   </Auth0Provider>,
