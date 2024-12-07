cba8c47e8ef4deb84139d5c16f0e8a48da38396b
Jakob-SA <agergaardskovjakob@gmail.com>
2024-12-07 14:28:22 +0100

    Log out button now redirects to correct frontpage and not localhost

--- Frontend/src/auth0Components.tsx
+++ Frontend/src/auth0Components.tsx
@@ -1,35 +1,37 @@
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
-    return (
-        <button
-            className="mt-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 text-xs rounded"
-            onClick={() => loginWithRedirect()}
-        >
-            Log In
-        </button>
-    );
+  return (
+    <button
+      className="mt-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 text-xs rounded"
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </button>
+  );
 };
 
 const LogoutButton = () => {
-    const { logout } = useAuth0();
+  const { logout } = useAuth0();
 
-    return (
-        <button
-            className="mt-1 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 text-xs rounded"
-            onClick={() => logout()}
-        >
-            Log Out
-        </button>
-    );
-}
+  return (
+    <button
+      className="mt-1 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 text-xs rounded"
+      onClick={() =>
+        logout({ logoutParams: { returnTo: window.location.origin } })
+      }
+    >
+      Log Out
+    </button>
+  );
+};
 
 const AuthenticationButton = () => {
-    const { isAuthenticated } = useAuth0();
-  
-    return isAuthenticated ? <LogoutButton /> : <LoginButton />;
-  };
-  
-  export default AuthenticationButton;
+  const { isAuthenticated } = useAuth0();
+
+  return isAuthenticated ? <LogoutButton /> : <LoginButton />;
+};
+
+export default AuthenticationButton;
